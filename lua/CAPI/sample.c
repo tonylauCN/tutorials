379f35c2362dfebbc6f8e96586e1a5ffe0baf18c
ryuu.kk <ryuu.pro@gmail.com>
2018-02-22 22:19:07 +0800

    add CAPI sample

--- lua/CAPI/sample.c
+++ lua/CAPI/sample.c
@@ -1,16 +1,28 @@
 
-
 #include <stdio.h>
 #include <string.h>
-//#include "lua.h"
-//#include "luaconf.h"
-//#include "lauxlib.h"
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
 
 int main(void)
 {
-    char buff[256];
-    int error;
+    char buff[256] = "error";
+    int error = 0;
     lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
 
+    while (fgets(buff, sizeof(buff), stdin) != NULL)
+    {
+        error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
 
+        if (error)
+        {
+            fprintf(stderr, "%s", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
+    }
+    lua_close(L);
+    printf("===>%d", error);
+    return 0;
 }
